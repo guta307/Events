3769d381c1760efbd9f8771f7aa5a9bfa6b0d816
guta307 <veiga187@gmail.com>
2023-12-16 11:32:31 -0300

    refactor: Implement inheritance for Sequelize model methods
    This commit introduces a significant refactor to our controller structure. We have created a base class (`BaseController`) which encapsulates common Sequelize model methods like `create`, `find`, and `listAll`. The specific model to be used by these methods is now determined by a protected static attribute, `SequelizeModel`, defined in the base class.
    
    Each derived controller class (e.g., `UserController`, `EventController`) simply overrides this static attribute to specify its corresponding Sequelize model, such as `Person` or `Event`. This design leverages inheritance and the protected modifier for encapsulation, ensuring that only the base class and its subclasses have access to the `SequelizeModel` attribute.
    
    This refactor enhances code reusability, maintainability, and follows best practices in object-oriented programming, especially in the context of our TypeScript application's architecture.

--- src/routes/eventRoute.ts
+++ src/routes/eventRoute.ts
@@ -1,15 +1,19 @@
 import { Router } from "express";
 import { EventController } from "../controllers/eventController/index.js";
+
 const router = Router();
 
-router.post("/event/create", EventController.create);
+router.post("/event/create", EventController.create.bind(EventController));
 
-router.get("/event/listAll", EventController.listAll);
+router.get("/event/listAll", EventController.listAll.bind(EventController));
 
-router.post("/event/findEvent", EventController.find);
+router.post("/event/findEvent", EventController.find.bind(EventController));
 
-router.put("/event/update/:id", EventController.update);
+router.put("/event/update/:id", EventController.update.bind(EventController));
 
-router.delete("/event/delete/:id", EventController.delete);
+router.delete(
+  "/event/delete/:id",
+  EventController.delete.bind(EventController)
+);
 
 export default router;
